06f3d9c6d338369bdc10eb9e55e1318954818692
Ryan King <marcidius@hotmail.com>
2016-09-19 21:28:32 -0400

    Clean run with no errors.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,3 +18,10 @@ import { AppComponent } from './app.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+// to get the angular cli to work without throwing errors in Webstorm:
+// 1. remove "lib": ["es6, dom"] from tsconfig
+// 2. installed @types - "npm install --save-dev @types/core-js
+// 3. add "declare var require: any;" to typings.d.ts
+// 4. add "typescript_tsdk": "/usr/local/lib/node_modules/typescript/lib", to tsconfig
